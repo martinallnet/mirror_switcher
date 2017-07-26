3bed9528015c78c6399c362feb5ddaa2ea851d8c
martinallnet <martinallnet@gmail.com>
2017-07-26 19:53:55 +0200

    Update switch_mirror.sh

--- switch_mirror.sh
+++ switch_mirror.sh
@@ -5,13 +5,17 @@ sources_local="/etc/apt/sources.list.local"
 sources_network="/etc/apt/sources.list.network"
 sources_global="/etc/apt/sources.list.global"
 
+mirrors="/etc/apt/sources.list"
+mirrors_network="/etc/apt/sources.list.network"
+mirrors_global="/etc/apt/sources.list.global"
+
 if [ "$1" = "local" ]; then
 
  if [ -f $sources_local ]; then
   cp $sources_local $sources
   echo "OK"
  elif [ ! -f $sources_local ];then
-  echo "Error: No local mirror source avaiable!"
+  echo "Error: No local mirror source file avaiable!"
  fi
 
 elif [ "$1" = "network" ]; then
@@ -20,7 +24,7 @@ elif [ "$1" = "network" ]; then
   cp $sources_network $sources
   echo "OK"
  elif [ ! -f $sources_network ];then
-  echo "Error: No network mirror source avaiable!"
+  echo "Error: No network mirror source file avaiable!"
  fi
 
 elif [ "$1" = "global" ]; then
@@ -29,12 +33,34 @@ elif [ "$1" = "global" ]; then
   cp $sources_global $sources
   echo "OK"
  elif [ ! -f $sources_global ];then
-  echo "Error: No global mirror source avaiable!"
+  echo "Error: No global mirror source file avaiable!"
+ fi
+
+elif [ "$1" = "m-network" ]; then
+
+ if [ -f $mirrors_network ]; then
+  cp $mirrors_network $mirrors
+  echo "OK"
+ elif [ ! -f $mirrors_network ];then
+  echo "Error: No network apt-mirror source file avaiable!"
+ fi
+
+elif [ "$1" = "m-global" ]; then
+
+ if [ -f $mirrors_global ]; then
+  cp $mirrors_global $mirrors
+  echo "OK"
+ elif [ ! -f $mirrors_global ];then
+  echo "Error: No global apt-mirror mirror source file avaiable!"
  fi
 
 else
- echo "$0 local     local mirror source"
- echo "$0 network   network mirror source"
- echo "$0 global    global mirror source"
+ echo "$0 local       local mirror source"
+ echo "$0 network     network mirror source"
+ echo "$0 global      global mirror source"
+ echo.
+ echo "$0 m-network   network apt-mirror source"
+ echo "$0 m-global    global apt-mirror source"
+
 
 fi
